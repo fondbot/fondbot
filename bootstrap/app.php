b0490f983a1154d6f4a008be54477214b0305433
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-12 17:18:53 +0300

    Introducing Toolbelt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,3 @@ $container = new League\Container\Container();
 $container->addServiceProvider(new App\Providers\ConfigServiceProvider);
 $container->addServiceProvider(new App\Providers\CacheServiceProvider);
 $container->addServiceProvider(new App\Providers\QueueServiceProvider);
-
-$kernel = FondBot\Application\Factory::create(
-    $container,
-    __DIR__.'/..',
-    __DIR__.'/../resources/fondbot',
-    ''
-);
--- public/index.php
+++ public/index.php
@@ -2,6 +2,13 @@
 
 require __DIR__.'/../bootstrap/app.php';
 
+$kernel = FondBot\Application\Factory::create(
+    $container,
+    __DIR__.'/..',
+    __DIR__.'/../resources/fondbot',
+    ''
+);
+
 /** @var League\Route\RouteCollection $router */
 $router = $kernel->resolve('router');
 /** @var Zend\Diactoros\Response\SapiEmitter $emitter */
