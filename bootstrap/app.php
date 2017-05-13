5d2b83bc4b264dde26d5e9ab41b4fa4baaebf41a
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-14 01:23:41 +0300

    refactor configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,11 @@ require __DIR__.'/../vendor/autoload.php';
 
 $container = new League\Container\Container();
 
-$container->addServiceProvider(new App\Providers\ConfigServiceProvider);
+$container->share(FondBot\Application\Config::class, function () {
+    return new FondBot\Application\Config(
+        require __DIR__.'/../config/app.php'
+    );
+});
+
 $container->addServiceProvider(new App\Providers\CacheServiceProvider);
 $container->addServiceProvider(new App\Providers\QueueServiceProvider);
