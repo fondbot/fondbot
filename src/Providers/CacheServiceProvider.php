d570229c636ae8f90e2f7926cf150701c20bef80
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-16 00:24:03 +0300

    share adapters to container

--- src/Providers/CacheServiceProvider.php
+++ src/Providers/CacheServiceProvider.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use FondBot\Contracts\Cache;
-use League\Flysystem\Filesystem;
-use FondBot\Cache\FilesystemCache;
-use League\Flysystem\Adapter\Local;
+use FondBot\Cache\Adapter;
+use FondBot\Cache\Adapters\FilesystemAdapter;
 use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
 
 class CacheServiceProvider extends AbstractServiceProvider
 {
     protected $provides = [
-        Cache::class,
+        Adapter::class,
     ];
 
     protected $path = '/resources/fondbot/cache';
@@ -27,12 +27,12 @@ class CacheServiceProvider extends AbstractServiceProvider
      */
     public function register(): void
     {
-        $this->getContainer()->share(Cache::class, function () {
+        $this->getContainer()->share(Adapter::class, function () {
             $filesystem = new Filesystem(
                 new Local($this->getContainer()->get('base_path').$this->path)
             );
 
-            return new FilesystemCache($filesystem);
+            return new FilesystemAdapter($filesystem);
         });
     }
 }
