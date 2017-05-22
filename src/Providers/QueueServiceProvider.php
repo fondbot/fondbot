34486503185ed5ecf50539bcdfcb71ea9e811482
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-23 02:11:53 +0300

    remove provides from QueueServiceProvider

--- src/Providers/QueueServiceProvider.php
+++ src/Providers/QueueServiceProvider.php
@@ -5,16 +5,11 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use FondBot\Queue\Adapter;
-use FondBot\Contracts\Queue;
 use FondBot\Queue\Adapters\SyncAdapter;
 use FondBot\Queue\QueueServiceProvider as BaseQueueServiceProvider;
 
 class QueueServiceProvider extends BaseQueueServiceProvider
 {
-    protected $provides = [
-        Queue::class,
-    ];
-
     /**
      * Queue adapter.
      *
