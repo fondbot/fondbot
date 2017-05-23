d2d2b6146f8143fc69bd47d53d08fbf7733a2400
Vladimir Yuldashev <misterio92@gmail.com>
2017-05-24 00:29:56 +0300

    ToolbeltServiceProvider

---
+++ src/Providers/ToolbeltServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use FondBot\Toolbelt\Command;
+use FondBot\Toolbelt\ToolbeltServiceProvider as BaseToolbeltServiceProvider;
+
+class ToolbeltServiceProvider extends BaseToolbeltServiceProvider
+{
+    /**
+     * Console commands.
+     *
+     * @return Command[]
+     */
+    public function commands(): array
+    {
+        return [];
+    }
+}
