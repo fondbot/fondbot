400a0d761d6684bcafb55ee11b7c8bc9fe74eb25
vyuldashev <misterio92@gmail.com>
2017-08-30 01:14:53 +0300

    application folder changed from src to app

--- app/Intents/ExampleIntent.php
+++ app/Intents/ExampleIntent.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Bot\Intents;
 
 use FondBot\Conversation\Intent;
-use FondBot\Drivers\ReceivedMessage;
 use FondBot\Conversation\Activators\Activator;
+use FondBot\Events\MessageReceived;
 
 class ExampleIntent extends Intent
 {
@@ -24,7 +24,7 @@ class ExampleIntent extends Intent
         ];
     }
 
-    public function run(ReceivedMessage $message): void
+    public function run(MessageReceived $message): void
     {
         $this->sendMessage('Hello, '.$this->getUser()->getUsername().'!');
     }
