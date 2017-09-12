8be520d0b28975fab84082a3ccf95581153dab4e
vyuldashev <misterio92@gmail.com>
2017-09-12 12:02:42 +0300

    update ".env.example", add facebook to "ChannelServiceProvider"

--- app/Providers/ChannelServiceProvider.php
+++ app/Providers/ChannelServiceProvider.php
@@ -20,6 +20,12 @@ class ChannelServiceProvider extends ServiceProvider
                 'driver' => 'telegram',
                 'token' => env('TELEGRAM_TOKEN'),
             ],
+            'facebook' => [
+                'driver' => 'facebook',
+                'page_token' => env('FACEBOOK_PAGE_TOKEN'),
+                'verify_token' => env('FACEBOOK_VERIFY_TOKEN'),
+                'app_secret' => env('FACEBOOK_APP_SECRET'),
+            ],
         ];
     }
 }
